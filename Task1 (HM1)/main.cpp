7c2326c09706e6c6e66333c4cc5b4157c0bc1010
nikitademedyuk1@gmail.com <nikitademedyuk1@gmail.com>
2022-03-22 22:52:58 +0300

    final commit

--- Task1 (HM1)/main.cpp
+++ Task1 (HM1)/main.cpp
@@ -6,9 +6,40 @@
 //
 
 #include <iostream>
+#include <string>
+#include <cassert>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+void isItSuitable (const int &length1, const int &width1, const int &length2, const int &width2) {
+    std::cout << "\n------------------------------------------\n";
+    if ((length2 <= length1 && width2 <= width1) || (length2 <= width1 && width2 <= length1)) {
+        std::cout << "\nRectfngle can be placed into another rectangle!\n\n";
+    } else {
+        std::cout << "\nRectfngle can't be placed into another rectangle.\n\n ";
+    }
+}
+
+int main() {
+ 
+    std::cout << "\nEnter the length of outside rectangle:\nlength1 = ";
+    int length1 {0};
+    std::cin >> length1;
+    assert(length1 >= 0);
+    
+    std::cout << "\nEnter the width of outside rectangle:\nwidth1 = ";
+    int width1 {0};
+    std::cin >> width1;
+    assert(width1 >= 0);
+    
+    std::cout << "\nEnter the length of inside rectangle:\nlength2= ";
+    int length2 {0};
+    std::cin >> length2;
+    assert(length2 >= 0);
+    
+    std::cout << "\nEnter the width of inside rectangle:\nwidth2= ";
+    int width2 {0};
+    std::cin >> width2;
+    assert(width2 >= 0);
+    
+    isItSuitable(length1, width1, length2, width2);
     return 0;
 }
